82ad15445e7fc19afacb5391ba69512c276ead41
yuleicoco <1040341999@qq.com>
2016-11-08 10:10:12 +0800

    测试下

---
+++ LoginDemo/LoginDemo/AppDelegate.m
@@ -0,0 +1,197 @@
+//
+//  AppDelegate.m
+//  LoginDemo
+//
+//  Created by yulei on 16/10/21.
+//  Copyright © 2016年 yulei. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKConnector/ShareSDKConnector.h>
+
+//腾讯开放平台（对应QQ和QQ空间）SDK头文件
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+
+//微信SDK头文件
+#import "WXApi.h"
+
+//新浪微博SDK头文件
+#import "WeiboSDK.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    // 新浪
+    // Facebook
+    // 推特儿
+    // 微信
+    // QQ
+    
+    [ShareSDK registerApp:@"85d250655d35" activePlatforms:
+     @[@(SSDKPlatformTypeSinaWeibo),
+       @(SSDKPlatformTypeWechat),
+       @(SSDKPlatformTypeQQ)]onImport:^(SSDKPlatformType platformType) {
+           
+           switch (platformType)
+           {
+               case SSDKPlatformTypeWechat:
+                   [ShareSDKConnector connectWeChat:[WXApi class]];
+                   break;
+               case SSDKPlatformTypeQQ:
+                   [ShareSDKConnector connectQQ:[QQApiInterface class] tencentOAuthClass:[TencentOAuth class]];
+                   
+                   break;
+               case SSDKPlatformTypeSinaWeibo:
+                   [ShareSDKConnector connectWeibo:[WeiboSDK class]];
+                   break;
+                   
+               default:
+                   break;
+           }
+       } onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo) {
+           
+           
+           switch (platformType)
+           {
+               case SSDKPlatformTypeSinaWeibo:
+                   
+                   //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
+                   [appInfo SSDKSetupSinaWeiboByAppKey:@"449565157"
+                                            appSecret:@"5af4c81f3d1f17a2c7054fc0d6813796"
+                                           redirectUri:@"http://www.sharesdk.cn"
+                                              authType:SSDKAuthTypeBoth];
+                   break;
+               case SSDKPlatformTypeWechat:
+                   [appInfo SSDKSetupWeChatByAppId:@"wx33a86d5926a50bdd"
+                                         appSecret:@"6a3d1ca67089314014fc273f58ff960f"];
+                   break;
+                   
+               case SSDKPlatformTypeQQ:
+                   
+                   
+                   
+                   [appInfo SSDKSetupQQByAppId:@"1105649623"
+                                        appKey:@"ib1rJ6NEUIon3k0o"
+                                      authType:SSDKAuthTypeBoth];
+                   break;
+                   default:
+                   break;
+           }
+       }];
+    
+
+    
+    
+    return YES;
+}
+
+
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+
+
+    
+}
+
+
+
+
+//- (BOOL)application:(UIApplication *)application
+//      handleOpenURL:(NSURL *)url
+//{
+//    
+//    
+//}
+//
+//- (BOOL)application:(UIApplication *)application
+//            openURL:(NSURL *)url
+//  sourceApplication:(NSString *)sourceApplication
+//         annotation:(id)annotation
+//{
+//    
+//}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Saves changes in the application's managed object context before the application terminates.
+    [self saveContext];
+}
+
+
+#pragma mark - Core Data stack
+
+@synthesize persistentContainer = _persistentContainer;
+
+- (NSPersistentContainer *)persistentContainer {
+    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
+    @synchronized (self) {
+        if (_persistentContainer == nil) {
+            _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"LoginDemo"];
+            [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
+                if (error != nil) {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    
+                    /*
+                     Typical reasons for an error here include:
+                     * The parent directory does not exist, cannot be created, or disallows writing.
+                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                     * The device is out of space.
+                     * The store could not be migrated to the current model version.
+                     Check the error message to determine what the actual problem was.
+                    */
+                    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+                    abort();
+                }
+            }];
+        }
+    }
+    
+    return _persistentContainer;
+}
+
+#pragma mark - Core Data Saving support
+
+- (void)saveContext {
+    NSManagedObjectContext *context = self.persistentContainer.viewContext;
+    NSError *error = nil;
+    if ([context hasChanges] && ![context save:&error]) {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+        abort();
+    }
+}
+
+@end
---
+++ LoginDemo/LoginDemo/ViewController.m
@@ -0,0 +1,190 @@
+//
+//  ViewController.m
+//  LoginDemo
+//
+//  Created by yulei on 16/10/21.
+//  Copyright © 2016年 yulei. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <ShareSDKExtension/SSEThirdPartyLoginHelper.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface ViewController ()
+
+{
+    
+    UIButton * btn;
+    UIView * bigBtn;
+    CGPoint currentPoint;
+    
+    
+    
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    /*
+    UIButton * btn =[[UIButton alloc]init];
+    [btn addTarget:self action:@selector(TouchBtn:) forControlEvents:UIControlEventTouchUpInside];
+    btn.backgroundColor =[UIColor redColor];
+    btn.frame = CGRectMake(30, 100, 100, 100);
+    [btn setTitle:@"点我登录" forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+     */
+    
+    
+    bigBtn =[[UIView alloc]initWithFrame:CGRectMake(0, 0, 260, 260)];
+    bigBtn.backgroundColor =[UIColor blueColor];
+    bigBtn.center = self.view.center;
+    bigBtn.layer.masksToBounds = YES;
+    bigBtn.layer.cornerRadius = 130;
+    [self.view addSubview:bigBtn];
+    
+    
+    
+    btn =[[UIButton alloc]initWithFrame:CGRectMake(0, 0, 60, 60)];
+    btn.center = CGPointMake(bigBtn.bounds.size.width/2, bigBtn.bounds.size.height/2);
+    
+    btn.backgroundColor =[UIColor redColor];
+    btn.layer.masksToBounds = YES;
+    btn.layer.cornerRadius = 30;
+   // [btn addTarget:self action:@selector(touchSta) forControlEvents:UIControlEventTouchUpInside];
+    [bigBtn addSubview:btn];
+    
+   
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+ -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+    UITouch *touch = [touches anyObject];
+    NSUInteger toucheNum = [[event allTouches] count];//有几个手指触摸屏幕
+    if ( toucheNum > 1 ) {
+        return;//多个手指不执行旋转
+    }
+    
+    CGPoint center = CGPointMake(CGRectGetMidX([touch.view bounds]), CGRectGetMidY([touch.view bounds]));
+     currentPoint = [touch locationInView:touch.view];//当前手指的坐标
+    CGPoint previousPoint = [touch previousLocationInView:touch.view];//上一个坐标
+       
+    
+    [self touchSta:currentPoint];
+    
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    
+    
+    btn.center = CGPointMake(bigBtn.bounds.size.width/2 , bigBtn.bounds.size.height/2 );
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded]; //告知页面立刻刷新，系统立即调用updateConstraints
+    }];
+    
+}
+
+
+- (void)touchSta:(CGPoint )sender
+{
+    
+    btn.center =sender;
+    [UIView animateWithDuration:0.4 animations:^{
+        [self.view layoutIfNeeded]; //告知页面立刻刷新，系统立即调用updateConstraints
+    }];
+    
+    
+}
+
+
+
+
+
+
+- (void)TouchBtn:(UIButton *)sender
+
+{
+//    
+//    [SSEThirdPartyLoginHelper loginByPlatform:SSDKPlatformTypeWechat
+//                                   onUserSync:^(SSDKUser *user, SSEUserAssociateHandler associateHandler) {
+//                                       
+//                                       //在此回调中可以将社交平台用户信息与自身用户系统进行绑定，最后使用一个唯一用户标识来关联此用户信息。
+//                                       //在此示例中没有跟用户系统关联，则使用一个社交用户对应一个系统用户的方式。将社交用户的uid作为关联ID传入associateHandler。
+//                                       associateHandler (user.uid, user, user);
+//                                       NSLog(@"dd%@",user.rawData);
+//                                       NSLog(@"dd%@",user.credential);
+//                                       
+//                                   }
+//                                onLoginResult:^(SSDKResponseState state, SSEBaseUser *user, NSError *error) {
+//                                    
+//                                    if (state == SSDKResponseStateSuccess)
+//                                    {
+//                                        
+//                                        
+//                                        NSLog(@"Successful");
+//                                        
+//                                    }
+//                                    
+//                                }];
+//    
+    
+    
+
+    
+    [ShareSDK getUserInfo:SSDKPlatformTypeSinaWeibo
+           onStateChanged:^(SSDKResponseState state, SSDKUser *user, NSError *error)
+     {
+         
+        
+         if (state == SSDKResponseStateSuccess)
+         {
+             
+             NSLog(@"id=%@",user.uid);
+             NSLog(@"%@",user.credential);
+             NSLog(@"token=%@",user.credential.token);
+             NSLog(@"昵称=%@",user.nickname);
+             NSLog(@"头像%@",user.icon);
+             
+         }
+                  
+         else
+         {
+             NSLog(@"%@",error);
+         }
+         
+     }];
+    
+     
+     
+    
+    
+      
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
